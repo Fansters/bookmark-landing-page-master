6c3a8535e17b334cdd22abbe2c9777d45c85a46d
fansters <edmundseizentals@gmail.com>
2022-07-04 16:02:00 +0300

    working on ovarlay menu and animation

--- app/js/script.js
+++ app/js/script.js
@@ -4,16 +4,24 @@ const lineBottom = document.querySelector('.line3');
 const header = document.querySelector('.header')
 const bookmarkLogo = document.getElementById('bookmarkImg');
 const overlay = document.querySelector('.overlay');
+const fade = document.querySelectorAll('.has-fade');
 
 btn.addEventListener('click', function () {
    header.classList.toggle('open');
    if (header.classList.contains('open')) {
-      overlay.classList.remove('fade-out')
-      overlay.classList.add('fade-in')
+      fade.forEach(function (el) {
+         el.classList.remove('fade-out')
+         el.classList.add('fade-in')
+      })
+
       bookmarkLogo.src = 'images/logo-bookmark_mobile_overlay.svg'
    } else {
-      overlay.classList.remove('fade-in')
-      overlay.classList.add('fade-out')
+         fade.forEach(function (el) {
+            el.classList.add('fade-out')
+            el.classList.remove('fade-in')
+         })
+      // overlay.classList.remove('fade-in')
+      // overlay.classList.add('fade-out')
       bookmarkLogo.src = 'images/logo-bookmark.svg'
    }
    lineTop.classList.toggle('spanWhite');
