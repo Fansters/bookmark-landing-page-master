51b2d2895a29d9d577c354e52a14691fdfeea81a
fansters <edmundseizentals@gmail.com>
2022-07-04 13:27:55 +0300

    fixed burger menu change on click + added logo change on click for mobile

--- app/js/script.js
+++ app/js/script.js
@@ -1,10 +1,18 @@
-const btn = document.querySelector('.header__burger');
+const btn = document.getElementById('btnBurger');
 const lineTop = document.querySelector('.line1');
 const lineBottom = document.querySelector('.line3');
-
+// const bookmarkImg = document.getElementById('bookmarkImg');
+const header = document.querySelector('.header')
+// const logoOverlay = document.getElementById('bookmarkImg').src = 'images/logo-bookmark_mobile_overlay.svg'
+const logoNormal = document.getElementById('bookmarkImg');
 
 btn.addEventListener('click', function () {
-   btn.classList.toggle('open');
+   header.classList.toggle('open');
+   if (header.classList.contains('open')) {
+      logoNormal.src = 'images/logo-bookmark_mobile_overlay.svg'
+   } else {
+      logoNormal.src = 'images/logo-bookmark.svg'
+   }
    lineTop.classList.toggle('spanWhite');
    lineBottom.classList.toggle('spanWhite');
 })
