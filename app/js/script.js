c349263fb5885f45853173c2f0073141e17f02db
fansters <edmundseizentals@gmail.com>
2022-07-04 13:52:14 +0300

    added overlay animation

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,20 @@
 const btn = document.getElementById('btnBurger');
 const lineTop = document.querySelector('.line1');
 const lineBottom = document.querySelector('.line3');
-// const bookmarkImg = document.getElementById('bookmarkImg');
 const header = document.querySelector('.header')
-// const logoOverlay = document.getElementById('bookmarkImg').src = 'images/logo-bookmark_mobile_overlay.svg'
-const logoNormal = document.getElementById('bookmarkImg');
+const bookmarkLogo = document.getElementById('bookmarkImg');
+const overlay = document.querySelector('.overlay');
 
 btn.addEventListener('click', function () {
    header.classList.toggle('open');
    if (header.classList.contains('open')) {
-      logoNormal.src = 'images/logo-bookmark_mobile_overlay.svg'
+      overlay.classList.remove('fade-out')
+      overlay.classList.add('fade-in')
+      bookmarkLogo.src = 'images/logo-bookmark_mobile_overlay.svg'
    } else {
-      logoNormal.src = 'images/logo-bookmark.svg'
+      overlay.classList.remove('fade-in')
+      overlay.classList.add('fade-out')
+      bookmarkLogo.src = 'images/logo-bookmark.svg'
    }
    lineTop.classList.toggle('spanWhite');
    lineBottom.classList.toggle('spanWhite');
