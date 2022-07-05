248f1f0666d47a62742c1096395521ee83811d53
fansters <edmundseizentals@gmail.com>
2022-07-05 09:57:38 +0300

    added noscroll on open menu. checking live site on mobile

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,5 @@
 const btn = document.getElementById('btnBurger');
+const body = document.querySelector('body');
 const lineTop = document.querySelector('.line1');
 const lineBottom = document.querySelector('.line3');
 const header = document.querySelector('.header')
@@ -8,18 +9,19 @@ const fade = document.querySelectorAll('.has-fade');
 
 btn.addEventListener('click', function () {
    header.classList.toggle('open');
+   body.classList.toggle('no-scroll')
    if (header.classList.contains('open')) {
       fade.forEach(function (el) {
          el.classList.remove('fade-out')
          el.classList.add('fade-in')
       })
-
       bookmarkLogo.src = 'images/logo-bookmark_mobile_overlay.svg'
+      // body.classList.add('no-scroll')
    } else {
-         fade.forEach(function (el) {
-            el.classList.add('fade-out')
-            el.classList.remove('fade-in')
-         })
+      fade.forEach(function (el) {
+         el.classList.add('fade-out')
+         el.classList.remove('fade-in')
+      })
       // overlay.classList.remove('fade-in')
       // overlay.classList.add('fade-out')
       bookmarkLogo.src = 'images/logo-bookmark.svg'
